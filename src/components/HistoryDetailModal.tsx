b510edcf316725a782941988c57475047d7a4e19
benzavraar <br7p56y8ps@privaterelay.appleid.com>
2026-07-30 19:01:37 +0600

    Update HistoryDetailModal.tsx

--- src/components/HistoryDetailModal.tsx
+++ src/components/HistoryDetailModal.tsx
@@ -134,11 +134,6 @@ export default function HistoryDetailModal({
                 </div>
               )}
             </div>
-            </div>
-              ))}
-           </div>
-         </div>
-            )}
           </div>
 
           {/* Footer - Undo Button */}
